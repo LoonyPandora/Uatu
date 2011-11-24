38048de5d82a192227e264181b07850c3237c611
James Aitken <loonypandora@gmail.com>
2011-11-24 05:13:53 +0000

    This is JS, so use camelCase for internal consistency.

--- public/js/jquery.calendar.js
+++ public/js/jquery.calendar.js
@@ -7,30 +7,30 @@
         var o = {
             month: now.getMonth(),
             year:  now.getFullYear(),
-            day_names:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-            month_names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            dayNames:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         };
 
         $.extend(o, params);
 
         // To show 6 rows of 7 days, we need to calculate the 42 days
         // that wholly contain our current month
-        var start_date = new Date(o.year, o.month, 1);
+        var startDate = new Date(o.year, o.month, 1);
 
         // Find the Sunday closest to the 1st of our selected month
-        start_date.setDate(start_date.getDate()-start_date.getDay());
+        startDate.setDate(startDate.getDate()-startDate.getDay());
         
         // A calendar is semantically closest to a table. So lets show it as one.
         var table = '<table class="datepicker"><tr>';
         table += '<th colspan="2"><a href="#" id="prev-month">&larr;</a></th>';
-        table += '<th colspan="3" id="current-month" data-year="'+o.year+'" data-month="'+o.month+'">'+o.month_names[o.month]+' '+o.year+'</th>';
+        table += '<th colspan="3" id="current-month" data-year="'+o.year+'" data-month="'+o.month+'">'+o.monthNames[o.month]+' '+o.year+'</th>';
         table += '<th colspan="2"><a href="#" id="next-month">&rarr;</a></th>';
         table += '</tr><tr>';
 
         // Day of the week header
         table += '</tr><tr>';
         for (d=0; d<7; d++) {
-            table += '<th class="weekday">' + o.day_names[d] + '</th>';
+            table += '<th class="weekday">' + o.dayNames[d] + '</th>';
         }
         table += '</tr><tr>';
 
@@ -41,12 +41,12 @@
             }
 
             var td_class = 'current-month';
-            if (start_date.getMonth() != o.month) {
+            if (startDate.getMonth() != o.month) {
                 td_class = 'other-month';
             }
 
-            table += '<td class='+ td_class +'><a>'+ start_date.getDate() +'</a></td>';
-            start_date.setDate(start_date.getDate() + 1);
+            table += '<td class='+ td_class +'><a>'+ startDate.getDate() +'</a></td>';
+            startDate.setDate(startDate.getDate() + 1);
         }
 
         table += '</tr></table>';
