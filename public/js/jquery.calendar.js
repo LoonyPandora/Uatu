9f478655374d5e9300773ee78299dbe66b82218e
James Aitken <loonypandora@gmail.com>
2011-11-24 05:12:16 +0000

    Add sane defaults to date selection
    Also re-adds day of week header to table.

--- public/js/jquery.calendar.js
+++ public/js/jquery.calendar.js
@@ -2,7 +2,11 @@
    
     function calendarWidget(el, params) { 
 
+        // Default to todays date if we don't pass anything
+        var now = new Date();
         var o = {
+            month: now.getMonth(),
+            year:  now.getFullYear(),
             day_names:   ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
             month_names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         };
@@ -11,26 +15,33 @@
 
         // To show 6 rows of 7 days, we need to calculate the 42 days
         // that wholly contain our current month
-        var start_date = new Date(params.year, params.month, 1);
+        var start_date = new Date(o.year, o.month, 1);
 
         // Find the Sunday closest to the 1st of our selected month
         start_date.setDate(start_date.getDate()-start_date.getDay());
-
-        // Show the following 42 days from our start date. 6 rows of 7 days each
+        
+        // A calendar is semantically closest to a table. So lets show it as one.
         var table = '<table class="datepicker"><tr>';
         table += '<th colspan="2"><a href="#" id="prev-month">&larr;</a></th>';
         table += '<th colspan="3" id="current-month" data-year="'+o.year+'" data-month="'+o.month+'">'+o.month_names[o.month]+' '+o.year+'</th>';
         table += '<th colspan="2"><a href="#" id="next-month">&rarr;</a></th>';
         table += '</tr><tr>';
 
-        // Each individual day
+        // Day of the week header
+        table += '</tr><tr>';
+        for (d=0; d<7; d++) {
+            table += '<th class="weekday">' + o.day_names[d] + '</th>';
+        }
+        table += '</tr><tr>';
+
+        // Show the following 42 days from our start date. 6 rows of 7 days each
         for (d=0; d<42; d++) {
             if (d % 7 == 0 && d != 41 && d != 0) {
                 table += '</tr><tr>';
             }
 
             var td_class = 'current-month';
-            if (start_date.getMonth() != params.month) {
+            if (start_date.getMonth() != o.month) {
                 td_class = 'other-month';
             }
 
